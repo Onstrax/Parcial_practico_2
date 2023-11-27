66eebea1f77137b9d1235706645e81742fa73836
Juanes Alvarez <je.alvarezg1@uniandes.edu.co>
2023-11-27 09:52:08 -0500

    Visualizacion conferencia list component

--- src/app/conferencia/conferencia.module.ts
+++ src/app/conferencia/conferencia.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ConferenciaComponent } from './conferencia.component';
+import { ConferenciaListComponent } from './conferencia-list/conferencia-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ConferenciaComponent]
+  exports: [ConferenciaListComponent, ],
+  declarations: [ConferenciaListComponent, ]
 })
 export class ConferenciaModule { }
